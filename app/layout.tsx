6f7af1c922dd879a8cfe4afd2394bc401e5159ea
Varun Tupuri <varuntupuri123@gmail.com>
2024-01-02 12:22:27 -0500

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
+
 import './css/style.css'
 
 import { Inter, Architects_Daughter } from 'next/font/google'
+import Script from 'next/script'
 
 import Header from '@/components/ui/header'
 import Banner from '@/components/banner'
 import { UserProvider } from './contexts/UserContext'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -33,17 +36,17 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+
     <html lang="en">
+      
+   
       <body className={`${inter.variable} ${architects_daughter.variable} font-inter antialiased bg-gray-900 text-gray-200 tracking-tight`}>
         <div className="flex flex-col min-h-screen overflow-hidden">
-        
-
           {children}
-
-          {/* <Banner /> */}
         </div>
+        <GoogleTagManager gtmId="G-K9W8W577K7" />
+
       </body>
     </html>
   )
 }
- 
